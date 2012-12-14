9885d4dc0ea1366c608f8086667d5ac79a19dc68
jcserrano <jorgecamse@gmail.com>
2012-12-14 19:55:08 +0100

    Modified categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,19 +25,21 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    @category = params[:id] ? Category.find(params[:id]) :Category.new
     @category.attributes = params[:category]
-    if request.post?
-      respond_to do |format|
-        format.html { save_category }
-        format.js do 
-          @category.save
-          @article = Article.new
-          @article.categories << @category
-          return render(:partial => 'admin/content/categories')
+    if params[:category]
+      if request.post?
+        respond_to do |format|
+          format.html { save_category }
+          format.js do 
+            @category.save
+            @article = Article.new
+            @article.categories << @category
+            return render(:partial => 'admin/content/categories')
+          end
         end
+        return
       end
-      return
     end
     render 'new'
   end
